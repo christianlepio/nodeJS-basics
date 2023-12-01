e7c8383930ef3e54d89e80d5c69523da81403ef2
christianlepio <rylepio79@gmail.com>
2023-12-01 20:35:09 +0800

    add url to config/allowedOrigins for registration form (reactJS)

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@
 const allowedOrigins = [
     'https://www.yoursite.com', 
     'https://127.0.0.1:5500', 
-    'http://localhost:3500'
+    'http://localhost:3500',
+    'http://localhost:5173' //React-Login-Series/1-react-form_validation
 ]
 
 module.exports = allowedOrigins
